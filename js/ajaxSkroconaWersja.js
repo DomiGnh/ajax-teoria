c3b15891d681940251112a9b71327c3b938e3db1
Dominika Gnich <d.gnich@wp.pl>
2017-06-25 14:01:28 +0200

    commit

--- js/ajaxSkroconaWersja.js
+++ js/ajaxSkroconaWersja.js
@@ -2,46 +2,54 @@
 
 
 //tworzymy funkcje
-function pobierzDane(){
-    
+function pobierzDane() {
+
     //tworzymy obiekt
     var httpReq = new XMLHttpRequest();
-    
-    
+
+
     //otwieramy połaczenie z serwerem , definiujemy gdzie wysyłąmy
-    httpReq.open("GET", "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", true );
-    
-    
+    httpReq.open("GET", "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", true);
+
+
     //tworzymy funkcje, w ktorej definiujemy co ma sie wydarzyc gdy serwer odpowie
-    httpReq.onreadystatechange = function(){
-        
+    httpReq.onreadystatechange = function () {
+
         // jak dane sa gotowe to ich status zmienia sie na 4
-        if (httpReq.readyState == 4){
-            
-            
-            if(httpReq.status >=200 && httpReq.status < 300 || httpReq.status == 304 || navigator.userAgent.indexOf('Safari') >= 0 && typeof httpReq.status == "undefined"){
-                
-                
+        if (httpReq.readyState == 4) {
+
+
+            if (httpReq.status >= 200 && httpReq.status < 300 || httpReq.status == 304 || navigator.userAgent.indexOf('Safari') >= 0 && typeof httpReq.status == "undefined") {
+
+
                 console.log(httpReq.responseText);
-                var returnData=JSON.parse(httpReq.responseText);
-                
-                
+                var returnData = JSON.parse(httpReq.responseText);
+
+
                 console.log(returnData);
-                
+
                 var userName = document.createElement('p');
                 userName.innerHTML = 'nazwa użytkowanika to : ' + returnData.userName;
                 document.body.appendChild(userName);
                 
-                console.log(userName);
+                var userURL = document.createElement('p');
+                userURL.innerHTML = 'url użytkowanika to : <a href=https://' + returnData.userURL + '>' + returnData.userURL + '</a>';
+                document.body.appendChild(userURL);
                 
-            httpReq = null; // nullem przerywamy połączenie
+                var userId = document.createElement('p');
+                userId.innerHTML = 'ID użytkowanika to : ' + returnData.userId;
+                document.body.appendChild(userId);
+
+                console.log(userName);
+
+                httpReq = null; // nullem przerywamy połączenie
             }
         }
     }
-    
+
     //wsyłamy, połcz z serwerem/ inicjowanie połączenia
     httpReq.send();
+
 }
 
-//wywylujemy funkcje
-pobierzDane();
+
